f1f9fb3c6519e0a2bd226aa054f25c863770de30
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-18 03:11:23 +0400

    KS-5-8 Jsoup Parser

---
+++ src/main/kotlin/jsoup_parser.kt
@@ -0,0 +1,15 @@
+package org.example
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
+fun main() {
+    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+    val quotes: Elements = doc.select("#__next div .sc-2aegk7-2")
+
+    for (quote: Element in quotes) {
+        println(quote.text())
+    }
+}
