0d375468e6bdd0709c1262b957026ea4161e1485
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-20 15:03:24 +0400

    KS-5-8 refactor tag

--- src/main/kotlin/jsoup_parser.kt
+++ src/main/kotlin/jsoup_parser.kt
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements
 
 fun main() {
     val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
-    val quotes: Elements = doc.select("#__next article")
+    val quotes: Elements = doc.select("article")
 
     for (quote: Element in quotes) {
         println(quote.text())
